b5d6ee115d17f555b983d2fbd5378e0da5e8e4e0
raniyaptla <ra.niyapoonthala@gmail.com>
2024-01-21 19:47:26 +0530

    updated

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,8 +6,46 @@ import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
-class Carousel extends Component {
-  
-}
 
-export default Carousel;
+class Carousel extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        currImg: 0,
+      };
+    }
+  
+    render() {
+      return(
+        <div className="carousel">
+        <div
+          className="carouselInner"
+          style={{ backgroundImage: `url(${images[this.state.currImg].img})` }}
+        >
+          <div
+            className="left"
+            onClick={() => {
+              this.state.currImg > 0 && this.setState({currImg:this.state.currImg - 1});
+            }}
+          >
+            <ArrowBackIosIcon style={{ fontSize: 50 }} />
+          </div>
+          <div className="center">
+            <h1>{images[this.state.currImg].title}</h1>
+            <p>{images[this.state.currImg].subtitle}</p>
+          </div>
+          <div
+            className="right"
+            onClick={() => {
+              this.state.currImg < images.length - 1 && this.setState({currImg:this.state.currImg + 1});
+            }}
+          >
+            <ArrowForwardIosIcon style={{ fontSize: 50 }} />
+          </div>
+        </div>
+      </div>
+      )
+    }
+  }
+  
+  export default Carousel;
